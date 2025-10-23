4825feca269a6e6105d9947e2ff647b9b34e8944
ansgrb <ansgrb@gmail.com>
2025-10-23 14:23:33 +0300

    Guards in action

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -6,9 +6,15 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log(`AuthGuard invoked`);
+    console.log(`Checking API key...`);
     const request = context.switchToHttp().getRequest();
     const apiKey = request.header('x-api-key');
-    console.log(`apiKey: ${apiKey}`);
+    if (apiKey !== `BondJamesBond`) {
+      console.log(`Invalid API key: ${apiKey}`);
+      return false;
+    }
+    console.log(`Valid API key: ${apiKey}`);
     return true;
   }
 }
