62698bf85245559d152ec82cbd6d477e43089a42
ansgrb <ansgrb@gmail.com>
2025-10-23 12:23:39 +0300

    Init random emoji

---
+++ src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getEmoji(): string {
+    const emojis = this.getEmojis();
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    return emojis[randomIndex];
+  }
+  getEmojis() {
+    return [`😀`, `🎉`, `🚀`, `🌟`, `🔥`];
+  }
+}
