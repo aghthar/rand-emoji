e65debf9bde40ca7d941492c21c8b5b3ad5ed165
ansgrb <ansgrb@gmail.com>
2025-10-25 13:16:01 +0300

    Wrap the response in a data object by adding a new Interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EmojiValidationPipe } from './common/emoji-validation/emoji-validation.pipe';
+import express from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getEmoji(@Query(`index`, EmojiValidationPipe) index?: number): string {
-    return this.appService.getEmoji(index);
+  getEmoji(
+    @Req() request: express.Request,
+    @Query(`index`, EmojiValidationPipe) index?: number,
+  ) {
+    return {
+      emoji: this.appService.getEmoji(index),
+      browser: request.headers.browser || 'Unknown',
+    };
   }
 }
