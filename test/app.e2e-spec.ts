7cf5f99bf3ee6b618a64bb3d7e3a60eca527eadb
ansgrb <ansgrb@gmail.com>
2025-10-26 15:40:51 +0300

    Add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,6 +6,7 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let server: any;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,13 +14,87 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    server = app.getHttpServer();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe(`/ GET`, () => {
+    // when valid api key return 200
+    it('/ (GET) with valid API key', () => {
+      return request(server)
+        .get('/')
+        .set('x-api-key', 'BondJamesBond')
+        .expect(200);
+    });
+
+    // when invalid api key return 403
+    it('/ (GET) with invalid API key', () => {
+      return request(server).get('/').set('x-api-key', 'invalid').expect(403);
+    });
+
+    // when no api key return 403
+    it('/ (GET) with no API key', () => {
+      return request(server).get('/').expect(403);
+    });
+
+    // / GET with valid api key should return a data object with random emoji
+    it('/ (GET) with valid API key should return a data object with random emoji', () => {
+      return request(server)
+        .get('/')
+        .set('x-api-key', 'BondJamesBond')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            data: {
+              emoji: expect.any(String),
+              browser: 'Unknown',
+            },
+          });
+        });
+    });
+
+    // /?index=0 GET with valid api key should return a data object with the first emoji
+    it('/?index=0 (GET) with valid API key should return a data object with the first emoji', () => {
+      return request(server)
+        .get('/?index=0')
+        .set('x-api-key', 'BondJamesBond')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            data: {
+              emoji: '😀',
+              browser: 'Unknown',
+            },
+          });
+        });
+    });
+
+    // /?index=<index> and that index is out of range GET with valid api key should return 404
+    it('/?index=100 (GET) with valid API key should return 404', () => {
+      return request(server)
+        .get('/?index=100')
+        .set('x-api-key', 'BondJamesBond')
+        .expect(400);
+    });
+
+    // /?index=-1 GET with valid api key should return 400
+    it('/?index=-1 (GET) with valid API key should return 400', () => {
+      return request(server)
+        .get('/?index=-1')
+        .set('x-api-key', 'BondJamesBond')
+        .expect(400);
+    });
+
+    // /?index=abc GET with valid api key should return 400
+    it('/?index=abc (GET) with valid API key should return 400', () => {
+      return request(server)
+        .get('/?index=abc')
+        .set('x-api-key', 'BondJamesBond')
+        .expect(400);
+    });
+
+    afterEach(async () => {
+      await app.close();
+    });
   });
 });
